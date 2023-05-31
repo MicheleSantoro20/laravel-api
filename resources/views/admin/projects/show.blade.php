f7c15bedb294a902d4ca274b6c3599d235f3029a
Michele Santoro <michelesantoro2000@icloud.com>
2023-05-31 07:16:20 +0200

    first commit

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<h1>{{$project->title}}</h1>
+<small>Slug: {{$project->slug}}</small>
+<hr/>
+<h2> Type: {{$project->category?$project->category->name : "Nessuna Tipologia Assegnata a questa istanza"}}</h2>
+
+@foreach ($project->technologies as $technology)
+    <span class="badge rounded-pill text-bg-primary">{{$technology->name}}</span>
+@endforeach
+
+<p>{{$project->description}}</p>
+<a href="{{route('admin.projects.index')}}" class="btn btn-secondary">Torna alla lista</a>
+
+
+@endsection
